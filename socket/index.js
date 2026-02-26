3004366c6e2f145a9f534e57927def6cb948db88
Victor Safta <132021789+VictorSaf@users.noreply.github.com>
2026-02-26 18:52:26 +0200

    refactor(socket): creează socket/index.js cu auth middleware și handler mount

---
+++ socket/index.js
@@ -0,0 +1,34 @@
+// socket/index.js
+'use strict';
+
+const jwt = require('jsonwebtoken');
+const { getDb } = require('../db/init');
+const config = require('../config');
+
+const presenceHandlers = require('./handlers/presence');
+const roomHandlers     = require('./handlers/rooms');
+const messageHandlers  = require('./handlers/messages');
+
+function initSocket(io) {
+  // Auth middleware Socket.IO
+  io.use((socket, next) => {
+    const token = socket.handshake.auth.token;
+    if (!token) return next(new Error('Authentication required'));
+    try {
+      socket.user = jwt.verify(token, config.jwt.secret);
+      next();
+    } catch {
+      next(new Error('Invalid token'));
+    }
+  });
+
+  io.on('connection', (socket) => {
+    const db = getDb();
+
+    presenceHandlers.register(io, socket, db);
+    roomHandlers.register(io, socket, db);
+    messageHandlers.register(io, socket, db);
+  });
+}
+
+module.exports = { initSocket };
